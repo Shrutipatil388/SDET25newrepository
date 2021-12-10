0a157b082133dd00d499c8160f99a459a0ffbec3
mugal <mugal@192.168.0.204>
2021-12-10 06:15:16 +0530

    commit-1 added

---
+++ src/test/java/Framework/Demo1.java
@@ -0,0 +1,21 @@
+package Framework;
+
+import org.testng.annotations.Test;
+
+public class Demo1 {
+	public class Demo {
+		@Test
+		
+		public void Demo() 
+		
+		{
+			System.out.println("this is user 1 - script-1");
+			
+			 
+		}
+
+	}
+
+}
+
+
